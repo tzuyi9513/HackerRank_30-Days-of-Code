e704ba687e0fbe8d69686859ac6ae5d648cff6ad
tzuyi9513 <53924772+tzuyi9513@users.noreply.github.com>
2020-02-06 14:57:47 -0500

    Create Day0_HelloWorld.py

---
+++ Day0_HelloWorld.py
@@ -0,0 +1,12 @@
+#Task
+#save a line of input from stdin to a variable
+#print Hello, World. on a single line
+#finally print the value of your variable on a second line.
+
+
+# Read a full line of input from stdin and save it to our dynamically typed variable, input_string.
+input_string = input()
+
+# Print a string literal saying "Hello, World." to stdout.
+print('Hello, World.')
+print(input_string)
